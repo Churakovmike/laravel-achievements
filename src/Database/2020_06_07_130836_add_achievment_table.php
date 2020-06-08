927f8b6ca1f39a15b05a73be50a006453c244e0f
ChurakovMike <churakovmike@mail.ru>
2020-06-08 23:29:17 +0700

    + add migration with creating main table

--- src/Database/2020_06_07_130836_add_achievment_table.php
+++ src/Database/2020_06_07_130836_add_achievment_table.php
@@ -13,7 +13,26 @@ class AddAchievmentTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('achievments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255)->nullable(true)->comment('Achievment name');
+            $table->string('description', 512)->nullable(true)->comment('Achievment description');
+            $table->string('image', 255)->nullable(true)->comment('Achievment image');
+            $table->bigInteger('type_id')->unsigned()->nullable(false)->default(0)->comment('Achievment type');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('achievment_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255)->nullable(false)->comment('Achievment type name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::table('achievments', function (Blueprint $table) {
+            $table->foreign('type_id', 'achievment_types_id_fk')->references('id')->on('achievment_types');
+        });
     }
 
     /**
@@ -23,6 +42,10 @@ class AddAchievmentTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('achievments', function (Blueprint $table) {
+            $table->dropForeign('achievment_types_id_fk');
+        });
+        Schema::dropIfExists('achievments');
+        Schema::dropIfExists('achievment_types');
     }
 }

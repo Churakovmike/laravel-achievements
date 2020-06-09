7bd77b6eaa2fd07f82b7ba406cc64e2b12ce4e02
ChurakovMike <churakovmike@mail.ru>
2020-06-09 19:27:22 +0700

    + add declare strict types

--- src/AchievementServiceProvider.php
+++ src/AchievementServiceProvider.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace ChurakovMike\Achievement;
 
 use Illuminate\Support\ServiceProvider;
--- src/Models/Achievment.php
+++ src/Models/Achievment.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace ChurakovMike\Achievement\Models;
 
 use Illuminate\Database\Eloquent\Model;
